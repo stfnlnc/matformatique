277c223d49c047b251754f5ba5f58123589fd9ca
Stefan Lancelot <hello@stefanlancelot.com>
2026-04-16 15:50:14 +0200

    add MainController + route + view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::name('main.')->group(function () {
+    Route::get('/', [MainController::class, 'index'])->name('home');
 });
